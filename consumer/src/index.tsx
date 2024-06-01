d46b58644ba93fc30514771df754c219c18b43fc
asaf.shochet <asaf.shochet@evinced.com>
2024-06-01 22:18:29 +0300

    bundle anazlyzer

--- consumer/src/index.tsx
+++ consumer/src/index.tsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 
 const App = () => (
   <div>
-    <h1>Hello, React 18 with TypeScript!</h1>
+    <h1>The best app</h1>
+    <div>AI inside, security too, block chain, web 3, you name it</div>
   </div>
 );
 
--- consumer/webpack.config.js
+++ consumer/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -23,6 +25,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
+    new BundleAnalyzerPlugin(),
   ],
   devServer: {
     static: path.resolve(__dirname, "dist"),
