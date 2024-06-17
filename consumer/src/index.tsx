1a5d509e1903cf0a3ad52940428c25721491b7b6
asaf.shochet <asaf.shochet@evinced.com>
2024-06-17 20:06:23 +0300

    comlicate consumer

--- consumer/src/index.tsx
+++ consumer/src/index.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { MyList, MyButton } from "tree-shaking-lecture";
+import {
+  MyList,
+  MyButton,
+  MyDropdown,
+  MyDatePicker,
+} from "tree-shaking-lecture";
 
 const App = () => (
   <div>
     <h1>The best app</h1>
     <div>AI inside, security too, block chain, web 3, you name it</div>
-    <MyButton onClick={() => alert("clicked")}>Click me</MyButton>
+    <MyButton onClick={() => alert("clicked")}>Button 1</MyButton>
+    <MyButton onClick={() => alert("clicked")}>Button 2</MyButton>
+    <MyDropdown
+      options={[
+        { label: "Option 1", value: "Option 1" },
+        { label: "Option 2", value: "Option 2" },
+      ]}
+      onChange={(e) => console.log(e.target.value)}
+    />
+
+    <MyDatePicker
+      value={new Date().toISOString()}
+      onChange={(e) => console.log(e.target.value)}
+    />
   </div>
 );
 
